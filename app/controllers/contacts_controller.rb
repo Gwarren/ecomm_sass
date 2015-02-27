dfc01e68f36fa02633552e3a9cc527bb9ffc7c46
Grant Warren <Gwarren1715@gmail.com>
2015-02-27 00:23:21 +0000

    Fixed contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
   end 
   
   def create
-  end 
+    @contact = Contact.new(contact_params)
+  
+    if @contact.save 
+      flash[:success] = "Message Sent."
+      redirect_to new_contact_path, notice: "Message sent succesfully."
+    else
+      flash[:danger] = "Error occured message not sent"
+      redirect_to new_contact_path, notice:"Error Ocurred."
+    end
+  end
+
+  private 
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments) 
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224185809) do
+ActiveRecord::Schema.define(version: 20150227001703) do
 
   create_table "contacts", force: true do |t|
+    t.string "name"
+    t.string "email"
+    t.text   "comments"
   end
 
 end
