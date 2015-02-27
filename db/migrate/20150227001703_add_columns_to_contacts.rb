23efee149ce235ed71ef214d592f2e5a58a5c5f1
Grant Warren <Gwarren1715@gmail.com>
2015-02-27 17:55:36 +0000

    Working to enable sendgrid on mailer form.

--- db/migrate/20150227001703_add_columns_to_contacts.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToContacts < ActiveRecord::Migration
-  def change
-    add_column :contacts, :name, :string 
-    add_column :contacts, :email, :string  
-    add_column :contacts, :comments, :text
-  end
-end
